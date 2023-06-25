bc85f3841db8c5866b6bbcdfd0f76dccece6e23d
acurzon <acurzon@serif.com>
2023-06-25 22:24:14 +0100

    remove default text & add magic button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Adam Curzon"/>
+  <MagicButton></MagicButton>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MagicButton from './components/MagicButton.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    MagicButton
+}
 }
 </script>
 
---
+++ src/components/MagicButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="magic-button">Click Me</button>
+</template>
+
+<script>
+export default {
+    name: 'MagicButton',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+.magic-button {
+    color: #0077ee;
+}
+</style>
