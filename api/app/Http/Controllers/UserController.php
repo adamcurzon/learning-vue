e290b69fc780d8b73654852ff81d3ffe8f2f2e84
acurzon <acurzon@serif.com>
2023-06-27 00:28:32 +0100

    cookies & jwt

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class UserController extends Controller
+{
+    private string $key = "123";
+
+    /**
+     * @param Request $request
+     * @return string|null
+     */
+    public function index (Request $request): ?string
+    {
+        $payload = [
+            // Issuer (me)
+            'iss' => 'https://ec2.adamcurzon.co.uk/',
+            // Audience (user)
+            'aud' => $request->ip(),
+            // Issued at (now)
+            'iat' => Carbon::now()->timestamp,
+            // Expiry (1 day)
+            'exp' => Carbon::now()->addDays(1)->timestamp,
+            // Access token
+            'access_token' => "456"
+        ];
+        $response->withCookie(cookie('name', 'MyValue', $minutes));
+        $jwt = JWT::encode($payload, $this->key, 'HS256');
+        var_dump($jwt);
+
+        $decoded = JWT::decode($jwt, new Key($this->key, 'HS256'));
+        print_r($decoded);
+
+        return null;
+    }
+
+    public function cookie(){
+        return response('Hello World')->cookie(
+            'key', '123', '123', '/', 'https://ec2.adamcurzon.co.uk/',false,false
+        );
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfoController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('info', InfoController::class);
+Route::apiResource('jwt', UserController::class);
