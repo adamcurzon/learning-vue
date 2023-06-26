3f98e718383e3bf47b808dc681275df7d2b13047
acurzon <acurzon@serif.com>
2023-06-27 00:30:20 +0100

    route fix

--- api/routes/web.php
+++ api/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cookie', [UserController::class, 'cookie']);
+Route::get('api/cookie', [UserController::class, 'cookie']);
